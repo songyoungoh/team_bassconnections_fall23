1e03aaf54a25eb2713a873b58bdeaf61d3a86dc0
Dary <147575954+DaryLu0v0@users.noreply.github.com>
2023-12-04 02:33:37 -0500

    Update train.py

--- src/train.py
+++ src/train.py
@@ -15,22 +15,22 @@ with open('../config.yaml') as p:
 # preprocessing the data
 
 train_dataset = MLRSNetMultiLabelDataset({
-    "batch_size": config["batch_size"],
-    "shuffle": config["shuffle"],
-    "num_workers": config["num_workers"],
-    "data_dir": config["data_dir"],
-    "csv_file": config["train_csv_file"]
+    "batch_size": config["m_batch_size"],
+    "shuffle": config["m_shuffle"],
+    "num_workers": config["m_num_workers"],
+    "data_dir": config["m_data_dir"],
+    "csv_file": config["m_train_csv_file"]
 })
 train_dataset.transform = ResizeCenterCropFlipHVToTensor()
 
 # Use parameters from config for test dataset
 # Note: Assuming similar parameters for the test dataset, adjust if needed
 test_dataset = MLRSNetMultiLabelDataset({
-    "batch_size": config["batch_size"],
-    "shuffle": config["shuffle"],
-    "num_workers": config["num_workers"],
-    "data_dir": config["data_dir"],
-    "csv_file": config["test_csv_file"]
+    "batch_size": config["m_batch_size"],
+    "shuffle": config["m_shuffle"],
+    "num_workers": config["m_num_workers"],
+    "data_dir": config["m_data_dir"],
+    "csv_file": config["m_test_csv_file"]
 })
 test_dataset.transform = ResizeCenterCropToTensor()
 
@@ -40,17 +40,17 @@ print("Data size - Training:", len(train_dataset), ", Testing:", len(test_datase
 # Prepare the model
 model = ResNet50MultiLabel({
     "num_classes": 60,  # Assuming 60 classes, change if different
-    "learning_rate": config["learning_rate"],
-    "pretrained": config["pretrained"],
-    "threshold": config["threshold"],
-    "metrics": config["metrics"]
+    "learning_rate": config["m_learning_rate"],
+    "pretrained": config["m_pretrained"],
+    "threshold": config["m_threshold"],
+    "metrics": config["m_metrics"]
 })
 model.prepare()
 
 # Train and evaluate model
 model.train_and_evaluate_model(
     train_dataset=train_dataset,
-    epochs=config["epochs"],
+    epochs=config["m_epochs"],
     model_directory=model_directory,  # Define or load this variable
     val_dataset=test_dataset,
 )
